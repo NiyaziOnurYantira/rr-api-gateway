092a73976c1e4166f6ce838e1b21275c9ca72708
onur <no1.yantira@gmail.com>
2025-04-29 15:06:01 +0300

    Update backend-3

--- backend-3/backend-3.js
+++ backend-3/backend-3.js
@@ -5,9 +5,9 @@ const PORT = 3003;
 app.use(express.json());
 
 app.use((req, res) => {
-  res.send(`Response from Backend 1`);
+  res.send(`Response from Backend 3`);
 });
 
 app.listen(PORT, () => {
-  console.log(`Backend 1 running on port ${PORT}`);
+  console.log(`Backend 3 running on port ${PORT}`);
 });
